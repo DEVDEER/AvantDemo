a3a9b1116cc729966586933d7c50ff1e4bbb59de
devdeer-alex <alexander.schmidt@devdeer.com>
2021-01-26 13:59:23 +0100

    Added samples for AAD and B2C

---
+++ Ui/Ui.B2cDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ui.B2cDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
